db334083ce585bcdaee3b786068a520351c65c1e
Обезличенная УЗ для стажировки-направление frontend-разработ. 12 <cr_ovr_fr12@efko.ru>
2024-09-03 17:07:54 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+
+    var header = document.getElementById("menu-burger__header");
+    header.classList.add("open-menu");
+
+    var nav = document.getElementById("header__nav");
+    header.classList.add("open-menu");
+
+}
